f6ef993a98fe3dd79c5f78a01de815d787e84404
Gaukhar Abdizhapbarova <gauhar.abdizhapbarova@gmail.com>
2022-09-12 23:27:37 +0600

    Change to how X's and O's are displayed on the gameboard, add logic that does not allow to play in spots that were already taken, add function for getting player's choice of X or O;

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const gameBoardModule = (function() {
 })();
 
 const displayControllerModule = (function() {
+    const circleHTML = '<i class="fa-regular fa-circle"></i>';
+    const crossHTML = '<i class="fa-solid fa-xmark"></i>';
+
     const gridBoxes = Array.from(document.querySelectorAll('.grid__box'));
     let turn = 'X';
 
     function renderArrayToScreen() {
         for (let i = 0; i < gameBoardModule.gameBoard.length; i++) {
-            gridBoxes[i].textContent = gameBoardModule.gameBoard[i];
+            if(gameBoardModule.gameBoard[i] === 'X') {
+                gridBoxes[i].innerHTML = crossHTML; 
+            } else if(gameBoardModule.gameBoard[i] === 'O') {
+                gridBoxes[i].innerHTML = circleHTML;
+            }
         }
     };
 
@@ -19,19 +26,25 @@ const displayControllerModule = (function() {
         let idx = gridBoxes.findIndex(function(box) {
             return box === event.target;
         });
+        turn = turn === 'X' ? 'O' : 'X';
+        if(gameBoardModule.gameBoard[idx] === '') {
         gameBoardModule.gameBoard[idx] = turn; 
+        };
         renderArrayToScreen();
     };
     
 
-    return {renderArrayToScreen, handleTurn};
+    return {handleTurn};
 })();
 
 const createPlayer = (playerName, playerNumber, assignedXO) => {
     const getPlayerName = () => {
         console.log('This is the name of player ' + playerNumber + '.....' + playerName);
     }
-    return {getPlayerName, playerName, playerNumber, assignedXO};
+    const getPlayerMark = function() {
+        return this.assignedXO;
+    };
+    return {getPlayerName, getPlayerMark, playerName, playerNumber, assignedXO};
 }
 
 let Justin = createPlayer('Justin', 1, 'X');
